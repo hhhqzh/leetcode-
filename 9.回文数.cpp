f80fabe4cbbb800c14c016ccbcc7ab797d520e0c
hhhqzh <985388100@qq.com>
2020-06-15 13:25:35 +0800

    完成第9题

---
+++ 9.回文数.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x == 0)
+            return true;
+        if(x<0 || x % 10 == 0)
+            return false;
+        int right = 0;
+        while(x > right){
+            right = right * 10 + x % 10;
+            x /= 10;
+        }
+        return x == right || x == right / 10;
+    }
+};
