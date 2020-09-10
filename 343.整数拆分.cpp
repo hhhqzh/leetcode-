3abea0e7d194392d4696d9ff454370447693a07b
hhhqz <985388100@qq.com>
2020-09-10 14:07:39 +0800

    完成第343题

--- 343.整数拆分.cpp
+++ 343.整数拆分.cpp
@@ -18,3 +18,17 @@ public:
         return dp[n];
     }
 };
+
+class Solution { 
+public:
+    int integerBreak(int n) {
+        int *dp = new int[n + 1]();
+        dp[1] = 1;
+        for(int i = 2; i <= n; ++i){
+            for(int j = 1; j < i; ++j){
+                dp[i] = max(dp[i], max(j * (i - j), dp[j] * (i - j)));
+            }
+        }
+        return dp[n];
+    }
+};
