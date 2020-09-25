80f4123901c15216fd8b4d5169a98e3b060511f3
hhhqzh <985388100@qq.com>
2020-09-25 09:10:20 +0800

    完成第518题

--- 518.零钱兑换II.cpp
+++ 518.零钱兑换II.cpp
@@ -7,6 +7,7 @@ public:
             return 0;
         }
         int* dp = new int[amount + 1]();
+        // amount == 0时，组合情况为1，即空
         dp[0] = 1;
         for(auto coin: coins){
             for(int i = coin; i <= amount; ++i){
