3b954b1a2e9601c8929aa492fbb55bad9aee4e1a
hhhqzh <985388100@qq.com>
2020-10-25 09:06:58 +0800

    完成都845题

---
+++ 845.数组中的最长山脉.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int longestMountain(int[] A) {
+        if(A.length < 3 || A == null)
+            return 0;
+        int cnt = 0;
+        for(int i = 1; i < A.length - 1; ++i){
+            if(A[i - 1] < A[i] && A[i] > A[i + 1]){
+                int l = i - 1, r = i + 1;
+                while(l > 0 && A[l] > A[l - 1])
+                    --l;
+                while(r < A.length - 1 && A[r] > A[r + 1])
+                    ++r;
+                cnt = Math.max(cnt, r - l + 1);
+            }
+        }
+        return cnt;
+    }
+}
