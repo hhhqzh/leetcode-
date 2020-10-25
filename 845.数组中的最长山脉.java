7a8193271eadcb3d70421f29d2b9e3fd07bcdda8
hhhqzh <985388100@qq.com>
2020-10-25 09:18:13 +0800

    完成第845题

--- 845.数组中的最长山脉.java
+++ 845.数组中的最长山脉.java
@@ -1,3 +1,4 @@
+// 枚举山顶，再从山顶向左右两侧扩展找到山脚。
 class Solution {
     public int longestMountain(int[] A) {
         if(A.length < 3 || A == null)
@@ -15,4 +16,28 @@ class Solution {
         }
         return cnt;
     }
-} 
+}
+
+
+// 枚举山顶，用dp算出left[i]和right[i]
+// left[i]：索引比i小且比当前元素i小的元素数目
+// right[i]：索引比i大且比当前元素i大的元素数目
+class Solution {
+    public int longestMountain(int[] A) {
+        int n = A.length;
+        if(n < 3 || A == null)
+            return 0;
+        int []left = new int[n];
+        int []right = new int[n];
+        for(int i = 1; i < n; ++i)
+            left[i] = A[i - 1] < A[i] ? left[i - 1] + 1 : 0;
+        for(int i = n - 2; i >= 0; --i)
+            right[i] = A[i] > A[i + 1] ? right[i + 1] + 1 : 0;
+        int res = 0;
+        for(int i = 0; i < n; ++i){
+            if(left[i] > 0 && right[i] > 0)
+                res = Math.max(res, left[i] + right[i] + 1);
+        }
+        return res;
+    }
+}
