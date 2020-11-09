c5ffc516b90730ffbf74782d8e7aca541082595b
hhhqzh <985388100@qq.com>
2020-11-09 15:36:22 +0800

    完成第973题

---
+++ 973.最接近远点的K个点.java
@@ -0,0 +1,12 @@
+// java基础太差了！！
+class Solution {
+    public int[][] kClosest(int[][] points, int K) {
+        // 排序
+        Arrays.sort(points, new Comparator<int[]>() {
+            public int compare(int[] point1, int[] point2) {
+                return (point1[0] * point1[0] + point1[1] * point1[1]) - (point2[0] * point2[0] + point2[1] * point2[1]);
+            }
+        });
+        return Arrays.copyOfRange(points, 0, K);
+    }
+}
