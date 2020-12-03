ee9b5fc86abead92326a9c15195870ce467c3339
hhhqzh <985388100@qq.com>
2020-12-03 15:55:18 +0800

    第一次写基数排序，题解牛

---
+++ 164.最大间距.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        // 基数排序
+        int n = nums.length;
+        if(n < 2)
+            return 0;
+        long exp = 1;
+        int[] buf = new int[n];
+        int maxVal = Arrays.stream(nums).max().getAsInt();
+        while(maxVal >= exp){
+            int[] cnt = new int[10];
+            for(int i = 0; i < n; ++i){
+                int digit = (nums[i] / (int)exp) % 10;
+                ++cnt[digit];
+            }
+            for(int i = 1; i < 10; ++i)
+                cnt[i] += cnt[i - 1];
+            for(int i = n - 1; i >= 0; --i){
+                int digit = (nums[i] / (int)exp) % 10;
+                buf[cnt[digit] - 1] = nums[i];
+                --cnt[digit];
+            }
+            System.arraycopy(buf, 0, nums, 0, n);
+            exp *= 10;
+        }
+        int res = 0;
+        for(int i = 1; i < n; ++i)
+            res = Math.max(res, nums[i] - nums[i - 1]);
+        return res;
+    }
+}
