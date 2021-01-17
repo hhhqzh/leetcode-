57748340ade65d31a9fca6ee545bc97fadb31642
hhhqzh <985388100@qq.com>
2021-01-17 15:01:29 +0800

    完成了第1232题

---
+++ 1232.缀点成线.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+        if(coordinates.length == 2)
+            return true;
+        for(int i = 2; i < coordinates.length; ++i) {
+            if((coordinates[1][1] - coordinates[0][1]) * (coordinates[i][0] - coordinates[0][0]) != (coordinates[i][1] - coordinates[0][1]) * (coordinates[1][0] - coordinates[0][0]))
+                return false;
+        }
+        return true;
+    }
+}
