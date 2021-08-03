9e643ac484696e1c16f60665fe1857c8fdb84584
hhhqzh <985388100@qq.com>
2021-08-03 12:19:16 +0800

    完成第581题

--- 581.最短无序连续子数组.js
+++ 581.最短无序连续子数组.js
@@ -2,6 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
+// 排序
 var findUnsortedSubarray = function(nums) {
     if (nums.length == 1)
         return 0;
@@ -24,4 +25,25 @@ var findUnsortedSubarray = function(nums) {
     if (right < 0)
         return 0;
     return right - left + 1;
+};
+
+// 一次遍历 + 双指针
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findUnsortedSubarray = function(nums) {
+    var n = nums.length;
+    if (n == 1)
+        return 0;
+   let hight = 0, low = 0, max = nums[0], min = nums[n - 1];
+   for (let i = 0; i < n; ++i) {
+        max = Math.max(max, nums[i]);
+        min = Math.min(min, nums[n - i - 1]);
+        if (nums[i] < max)
+            hight = i;
+        if (nums[n - i - 1] > min)
+            low = n - i - 1;
+   }
+   return hight > low ? hight - low + 1 : 0;
 };
