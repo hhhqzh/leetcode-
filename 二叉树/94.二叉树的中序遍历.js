bc97e6d5cfa4924e1f74eab7dfc7629216c91789
hhhqzh <985388100@qq.com>
2021-08-17 14:05:50 +0800

    完成第94题

---
+++ 二叉树/94.二叉树的中序遍历.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ var inorderTraversal = function (root) {
+    var stack = [];
+    var res = [];
+    while (stack.length || root != null) {
+        if (root != null) {
+            stack.push(root);
+            root = root.left;
+        } else {
+            root = stack.pop();
+            res.push(root.val);
+            root = root.right;
+        }
+    }
+    return res;
+};
