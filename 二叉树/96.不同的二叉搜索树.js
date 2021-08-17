c921d3542903a4a51397ef48583aa9fc181919a0
hhhqzh <985388100@qq.com>
2021-08-17 14:10:42 +0800

    完成第96题

--- 二叉树/96.不同的二叉搜索树.js
+++ 二叉树/96.不同的二叉搜索树.js
@@ -9,8 +9,8 @@
     let dp = new Array(n + 1).fill(0);
     dp[0] = dp[1] = 1;
     for (let i = 2; i <= n; ++i) {
-        for (let j = 0; j < i; ++j) {
-            dp[i] += dp[j] * dp[i - j - 1];
+        for (let j = 1; j <= i; ++j) {
+            dp[i] += dp[j - 1] * dp[i - j];
         }
     }
     return dp[n];
