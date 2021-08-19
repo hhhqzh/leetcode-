4dc27f239469902d4e2b3b774a552dcccd6e2378
hhhqzh <985388100@qq.com>
2021-08-19 14:20:38 +0800

    完成第101题

---
+++ 二叉树/101.对称二叉树.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+ var isSymmetric = function(root) {
+    const dfs = (root1, root2) => {
+        if (root1 == null && root2 == null)
+            return true;
+        if (root1 == null || root2 == null)
+            return false;
+        if (root1.val == root2.val)
+            return dfs(root1.left, root2.right) && dfs(root1.right, root2.left);
+        return false;
+    }
+
+    return dfs(root.left, root.right);
+};
