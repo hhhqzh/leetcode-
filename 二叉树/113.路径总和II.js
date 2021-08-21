bbf33959d87693bd3b088716fc56ca949338d5bf
hhhqzh <985388100@qq.com>
2021-08-21 10:10:21 +0800

    完成第113题

---
+++ 二叉树/113.路径总和II.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number[][]}
+ */
+ var pathSum = function(root, targetSum) {
+    let sum = 0;
+    let res = [];
+    let arr = [];
+
+    const helper = (root, targetSum) => {
+        if (root == null)
+            return;
+        sum += root.val;
+        arr.push(root.val);
+        if (root.left == null && root.right == null && sum == targetSum) {
+            res.push([].concat(arr));
+        }
+        helper(root.left, targetSum);
+        helper(root.right, targetSum);
+        arr.pop();
+        sum -= root.val;
+    }
+
+    helper(root, targetSum);
+    return res;
+};
