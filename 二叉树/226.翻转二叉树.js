b02a7615dd87bcedf9c7e2bce76d418b4ca36211
hhhqzh <985388100@qq.com>
2021-08-27 20:40:40 +0800

    完成第226题

---
+++ 二叉树/226.翻转二叉树.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+ var invertTree = function(root) {
+    if (root == null)
+        return null;
+    let t = root.left;
+    root.left = root.right;
+    root.right = t;
+
+    invertTree(root.left);
+    invertTree(root.right);
+
+    return root;
+};
