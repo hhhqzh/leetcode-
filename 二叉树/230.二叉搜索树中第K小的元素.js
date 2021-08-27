efcb30c9da7e9003d5617ae663a7639df9837625
hhhqzh <985388100@qq.com>
2021-08-27 21:00:40 +0800

    完成第230题

---
+++ 二叉树/230.二叉搜索树中第K小的元素.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+ var kthSmallest = function(root, k) {
+
+    const getCount = (root) => {
+        if (root == null)
+            return 0;
+        return 1 + getCount(root.left) + getCount(root.right);
+    }
+
+    while (true) {
+        let l = getCount(root.left);
+        if (l >= k) {
+            root = root.left;
+        } else if (l == k - 1)
+            return root.val;
+        else {
+            root = root.right;
+            k = k - l - 1;
+        }
+    }
+};
