25d99d63ab9d971e406773b9eaed4b004fdff5af
hhhqzh <985388100@qq.com>
2021-09-01 10:51:40 +0800

    完成第437题

---
+++ 二叉树/437.路径总和III.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number}
+ */
+ var pathSum = function (root, targetSum) {
+    let s = 0;
+
+    const dfs =  (root) => {
+        if (root === null)
+            return;
+        Sum(root, 0);
+        dfs(root.left, targetSum);
+        dfs(root.right, targetSum);
+    }
+
+    const Sum = (root, sum) => {
+        if (root == null)
+            return;
+        sum += root.val;
+        if (targetSum == sum) {
+            ++s;
+        }
+        Sum(root.left, sum);
+        Sum(root.right, sum);
+    }
+
+    dfs(root);
+    return s;
+};
