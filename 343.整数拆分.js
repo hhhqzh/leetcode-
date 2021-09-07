1225d4e208edb45e03d85f784d48c6d926620bd4
hhhqzh <985388100@qq.com>
2021-09-07 21:33:37 +0800

    完成第343题

---
+++ 343.整数拆分.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var integerBreak = function(n) {
+    // dp[i] 为长度为i的最大拆分乘积
+    let dp = new Array(n + 1).fill(0);
+    dp[1] = 1;
+    for (let i = 2; i <= n; ++i) {
+        for (let j = 1; j < i; ++j) {
+            // 把长度为j分成一段
+            dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+        }
+    }
+    return dp[n];
+};
