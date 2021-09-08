7e58b225d55075cc369f0ebc93f811ecb42f967a
hhhqzh <985388100@qq.com>
2021-09-08 09:23:47 +0800

    完成第783题

---
+++ 二叉树/783. 二叉搜索树节点最小距离.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var minDiffInBST = function (root) {
+    let stack = [];
+    let res = Number.MAX_SAFE_INTEGER;
+    let pre = null;
+    while (root !== null || stack.length) {
+        while (root !== null) {
+            stack.push(root);
+            root = root.left;
+        }
+        if (stack.length) {
+            root = stack.pop();
+            if (pre !== null) {
+                res = Math.min(res, root.val - pre.val);
+            }
+            pre = root;
+            root = root.right;
+        }
+    }
+    return res;
+};
