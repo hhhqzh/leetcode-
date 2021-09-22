99a0044e11c4d22ec545e1d51d7b7828eef3ef08
hhhqzh <985388100@qq.com>
2021-09-22 11:31:36 +0800

    完成剑指offer第17题

---
+++ 剑指offer/17.打印从1到最大的n位数.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+ var printNumbers = function (n) {
+
+    let s = new Array(n).fill("");
+    let res = [];
+    const permutation = (s, pos) => {
+        if (pos == n) {
+            inputNumbers(s);
+            return;
+        }
+        for (let i = 0; i <= 9; ++i) {
+            s[pos] = i + "";
+            permutation(s, pos + 1);
+        }
+    }
+
+    const inputNumbers = (s) => {
+        let t = "";
+        let i = 0;
+        while (i < n && s[i] === "0")
+            ++i;
+        for (; i < n; ++i)
+            t += s[i];
+        if (t !== "")
+            res.push(parseInt(t));
+    }
+
+    permutation(s, 0);
+
+    return res;
+};
