fb224f3099080f50d7d39ee3d505ca325f6501db
hhhqzh <985388100@qq.com>
2021-09-26 15:43:53 +0800

    完成第371题

--- 371.两整数之和.js
+++ 371.两整数之和.js
@@ -1,1 +1,13 @@
-https://www.scholat.com/ytang
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+ var getSum = function(a, b) {
+    let sum, carry;
+    sum = a ^ b;
+    carry = (a & b) << 1;
+    if (carry !== 0)
+        return getSum(sum, carry);
+    return sum;
+};
