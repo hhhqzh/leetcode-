f62bed5203d8531166fa5ad75cfab48c9a38d4b0
hhhqzh <985388100@qq.com>
2021-09-29 11:40:40 +0800

    完成第33题

---
+++ 二分查找/33.搜索旋转排序数组.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var search = function (nums, target) {
+    let [l, r] = [0, nums.length - 1];
+    while (l <= r) {
+        let mid = Math.floor((r - l) / 2) + l;
+        if (nums[mid] === target)
+            return mid;
+        else if (nums[mid] < nums[r]) { // 右边是有序的
+            if (nums[mid] < target && nums[r] >= target) // target在右边
+                l = mid + 1;
+            else
+                r = mid - 1;
+        } else { // 左边是有序的
+            if (nums[mid] > target && nums[l] <= target) // target在左边
+                r = mid - 1;
+            else
+                l = mid + 1;
+        }
+    }
+    return -1;
+};
