d0d0f803b9fa109aa9e1ca700aa0d02996fae7f8
hhhqzh <985388100@qq.com>
2021-10-03 16:49:31 +0800

    完成第153题

---
+++ 二分查找/153.寻找旋转排序树组中的最小值.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var findMin = function(nums) {
+    let [l, r, mid] = [0, nums.length - 1];
+    while (l <= r) {
+        mid = Math.floor((r - l) / 2) + l;
+        if (nums[mid] < nums[r]) { // 右半部分是有序的
+            r = mid;
+        } else { // 左半部分是有序的
+            l = mid + 1;
+        }
+    }
+    return nums[mid];
+};
