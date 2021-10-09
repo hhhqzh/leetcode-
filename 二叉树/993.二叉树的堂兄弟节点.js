62397e971cc2a6c47229325016d9a5a1d25fa773
hhhqzh <985388100@qq.com>
2021-10-09 09:52:38 +0800

    完成第993题

---
+++ 二叉树/993.二叉树的堂兄弟节点.js
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} x
+ * @param {number} y
+ * @return {boolean}
+ */
+
+// bfs
+// var isCousins = function (root, x, y) {
+//     let height = new Array(101).fill(0);
+//     let father = new Array(101).fill(0);
+//     let queue = [root];
+//     while (queue.length) {
+//         let node = queue.shift();
+//         if (node.left) {
+//             height[node.left.val] = height[node.val] + 1;
+//             father[node.left.val] = node.val;
+//             queue.push(node.left);
+//         }
+//         if (node.right) {
+//             height[node.right.val] = height[node.val] + 1;
+//             father[node.right.val] = node.val;
+//             queue.push(node.right);
+//         }
+//     }
+//     return height[x] == height[y] && father[x] != father[y];
+// };
+
+
+// dfs 
+
+var isCousins = function (root, x, y) {
+    let xpar, xhei, ypar, yhei;
+
+    const dfs = (root, x, y, height, parent) => {
+        if (root === null)
+            return;
+        if (root.val === x) {
+            xpar = parent;
+            xhei = height;
+        } else if (root.val === y) {
+            ypar = parent;
+            yhei = height;
+        } else {
+            dfs(root.left, x, y, height + 1, root);
+            dfs(root.right, x, y, height + 1, root);
+        }
+    }
+
+    dfs(root.left, x, y, 1, root);
+    dfs(root.right, x, y, 1, root);
+
+    return xpar !== ypar && xhei === yhei;
+}
