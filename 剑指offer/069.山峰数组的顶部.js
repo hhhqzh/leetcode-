4835b6d19a95a119a10f935a7129036092a25fcf
hhhqzh <985388100@qq.com>
2021-10-14 10:15:37 +0800

    完成剑指offerII第69题

--- 剑指offer/069.山峰数组的顶部.js
+++ 剑指offer/069.山峰数组的顶部.js
@@ -2,16 +2,34 @@
  * @param {number[]} arr
  * @return {number}
  */
- var peakIndexInMountainArray = function (arr) {
-    let [l, r] = [0, arr.length - 1];
-    while (l < r) {
-        let mid = Math.floor((r - l) / 2) + l;
-        if (mid > 0 && mid < arr.length - 1 && arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+var peakIndexInMountainArray = function (arr) {
+    let low = 1,
+        high = arr.length - 2;
+    while (low <= high) {
+        let mid = low + Math.floor((high - low) / 2);
+        if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
             return mid;
-        } else if (mid > 0 && arr[mid] > arr[mid - 1]) {
-            l = mid;
-        } else if (mid < arr.length - 1 && arr[mid] > arr[mid + 1])
-            r = mid;
+        } else if (arr[mid] < arr[mid + 1]) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
     }
-    return l;
-};
+    return -1;
+}
+
+// 找到 最小满足 arr[i] > arr[i + 1] 的下标 i
+var peakIndexInMountainArray = function (arr) {
+    let l = 1,
+        r = arr.length - 2;
+    while (l <= r) {
+        let mid = Math.floor((r - l) / 2) + l;
+        if (arr[mid] > arr[mid + 1]) {
+            res = mid;
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return res;
+}
