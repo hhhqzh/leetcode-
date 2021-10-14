2770b5484b5b3ede45daf2462a229746d3eeaa5a
hhhqzh <985388100@qq.com>
2021-10-14 10:56:56 +0800

    完成dp第1137题

--- 动态规划dp/1137.第N个泰波那契数.js
+++ 动态规划dp/1137.第N个泰波那契数.js
@@ -7,13 +7,12 @@
         return 0;
     if (n == 1 || n == 2)
         return 1;
-    var a  = 0, b = 1, c = 1;
-    var res;
+    let a  = 0, b = 1, c = 1;
     for (let i = 3; i <= n; ++i) {
-        res = a + b + c;
+        let res = a + b + c;
         a = b;
         b = c;
         c = res;
     }
-    return res;
+    return c;
 };
