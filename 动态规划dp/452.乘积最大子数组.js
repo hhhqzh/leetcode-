44c4c070389d5e4c3cc7105cfe8abe25fc2d396b
hhhqzh <985388100@qq.com>
2021-10-21 11:33:48 +0800

    完成dp第452题

--- 动态规划dp/452.乘积最大子数组.js
+++ 动态规划dp/452.乘积最大子数组.js
@@ -2,7 +2,8 @@
  * @param {number[]} nums
  * @return {number}
  */
- var maxProduct = function (nums) {
+// dp
+var maxProduct = function (nums) {
     let preMax, max, preMin;
     preMax = max = preMin = nums[0];
     for (let i = 1; i < nums.length; ++i) {
@@ -17,3 +18,30 @@
     }
     return max;
 };
+
+/**
+ * 思路： 求最大值，可以看成求被0拆分的各个子数组的最大值。
+
+ * 当一个数组中没有0存在，则分为两种情况：
+
+ * 1.负数为偶数个，则整个数组的各个值相乘为最大值；
+
+ * 2.负数为奇数个，则从左边开始，乘到最后一个负数停止有一个“最大值”，从右边也有一个“最大值”，比较，得出最大值。
+ */
+var maxProduct = function (nums) {
+    let a = 1;
+    let max = nums[0];
+
+    for (let i = 0; i < nums.length; ++i) {
+        a = a * nums[i];
+        max = Math.max(max, a);
+        if (nums[i] == 0) a = 1;
+    }
+    a = 1;
+    for (let i = nums.length - 1; i >= 0; --i) {
+        a = a * nums[i];
+        max = Math.max(max, a);
+        if (nums[i] == 0) a = 1;
+    }
+    return max;
+}
