cfe99f0af34516808ef1c85524eb3b094372a060
hhhqzh <985388100@qq.com>
2021-10-23 20:23:34 +0800

    完成dp第122题

--- 动态规划dp/122.买卖股票的最佳时机II.js
+++ 动态规划dp/122.买卖股票的最佳时机II.js
@@ -9,4 +9,19 @@ var maxProfit = function (prices) {
             res += prices[i] - prices[i - 1];
     }
     return res;
-}; 
+};
+
+// 当天可以买入和卖出
+var maxProfit = function (prices) {
+    let pre = prices[0];
+    let res = 0;
+    for (let i = 1; i < prices.length; ++i) {
+        if (prices[i] > pre) {
+            res += prices[i] - pre;
+            pre = prices[i];
+        } else {
+            pre = prices[i];
+        }
+    }
+    return res;
+};
