003fb3a476c203af8d914501c6be6be558b47a45
hhhqzh <985388100@qq.com>
2021-10-27 11:36:26 +0800

    完成第301题

--- 301.删除无效的括号.js
+++ 301.删除无效的括号.js
@@ -43,3 +43,51 @@ var removeInvalidParentheses = function (s) {
         level = nextLevel;
     }
 };
+
+// DFS 剪枝
+// 对于每个 '('或')' 有两种情况，一种是添加，一种是不添加，遇到其他字符则添加。
+
+var removeInvalidParentheses = function (s) {
+    let len = 0; // 最后字符串的长度
+    let n = s.length;
+    let l = 0,
+        r = 0; // ( 和 ) 超出的个数
+    let c1 = 0,
+        c2 = 0; // ( 和 ) 的个数
+    for (let c of s) {
+        if (c === '(') {
+            ++l;
+            ++c1;
+        } else if (c === ')') {
+            if (l) --l;
+            else ++r;
+            ++c2;
+        }
+    }
+    len = n - l - r;
+    let max = Math.min(c1, c2); // 最终字符串中 ( 和 ) 最多的个数。
+    let res = new Set();
+
+    // index为当前访问的s的字符， cur当前的字符串，l为还需要去掉的(的个数，r为还需要去掉的)的个数
+    const dfs = (index, cur, l, r, score) => {
+        if (l < 0 || r < 0 || score < 0 || score > max)
+            return;
+        if (l === 0 && r === 0 && len === cur.length) {
+            res.add(cur);
+        }
+        if (index === n)
+            return;
+        let c = s.charAt(index);
+        if (c === '(') {
+            dfs(index + 1, cur + c, l, r, score + 1); // 添加 (
+            dfs(index + 1, cur, l - 1, r, score); // 删除 (
+        } else if (c === ')') {
+            dfs(index + 1, cur + c, l, r, score - 1); // 添加 )
+            dfs(index + 1, cur, l, r - 1, score); // 删除 )
+        } else {
+            dfs(index + 1, cur + c, l, r, score);
+        }
+    }
+    dfs(0, "", l, r, 0);
+    return Array.from(res);
+}
