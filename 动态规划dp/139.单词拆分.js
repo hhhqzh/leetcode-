216b1add00ca530bd39dc5428d6662ae3a01c90c
hhhqzh <985388100@qq.com>
2021-10-28 15:48:02 +0800

    完成dp第139题

--- 动态规划dp/139.单词拆分.js
+++ 动态规划dp/139.单词拆分.js
@@ -5,12 +5,28 @@
  */
 var wordBreak = function (s, wordDict) {
     let n = s.length;
+    // dp[i] 表示字符串 s 的前 i 个字符能否拆分成  wordDict 中的单词。
     let dp = new Array(n + 1).fill(false);
     dp[0] = true;
     for (let i = 1; i <= n; ++i) {
         for (let word of wordDict) {
             let len = word.length;
             if (len <= i && word === s.slice(i - len, i) && dp[i - len]) {
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+    return dp[n];
+};
+
+var wordBreak = function (s, wordDict) {
+    let n = s.length;
+    let dp = new Array(n + 1).fill(false);
+    dp[0] = true;
+    for (let i = 1; i <= n; ++i) {
+        for (let j = 0; j < i; ++j) {
+            if (wordDict.indexOf(s.slice(j, i)) !== -1 && dp[j]) {
                 dp[i] = true;
                 break;
             }
