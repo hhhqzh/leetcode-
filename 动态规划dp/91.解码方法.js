676857716741f63e2ea63743c250ba24604ef262
hhhqzh <985388100@qq.com>
2021-11-02 10:45:25 +0800

    完成dp第91题

--- 动态规划dp/91.解码方法.js
+++ 动态规划dp/91.解码方法.js
@@ -5,7 +5,7 @@
 var numDecodings = function (s) {
     let n = s.length;
     let dp = new Array(n + 1).fill(0);
-    dp[0] = 1;
+    dp[0] = 1; // 空字符串可以有 111 种解码方法，解码出一个空字符串。
     for (let i = 0; i < n; ++i) {
         if (s.charAt(i) !== '0')
             dp[i + 1] = dp[i];
