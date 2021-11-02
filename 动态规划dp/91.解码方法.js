36b4b3bf80d2b6af1a0c32c1b127d6b3974267ef
hhhqzh <985388100@qq.com>
2021-11-02 10:42:44 +0800

    完成dp第91题

---
+++ 动态规划dp/91.解码方法.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = function (s) {
+    let n = s.length;
+    let dp = new Array(n + 1).fill(0);
+    dp[0] = 1;
+    for (let i = 0; i < n; ++i) {
+        if (s.charAt(i) !== '0')
+            dp[i + 1] = dp[i];
+        // s[i - 1] 和 s[i] 映射成一个字母
+        if (i > 0 && (s.charAt(i - 1) === '1' || (s.charAt(i - 1) === '2' && s.charAt(i) <= '6')))
+            dp[i + 1] += dp[i - 1];
+    }
+    return dp[n];
+};
