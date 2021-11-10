246aa160ccb874a408a6cce812917b273483f267
hhhqzh <985388100@qq.com>
2021-11-10 11:04:58 +0800

    完成dp第63题（一维空间优化）

--- 动态规划dp/63.不同路径II.js
+++ 动态规划dp/63.不同路径II.js
@@ -27,3 +27,22 @@ var uniquePathsWithObstacles = function (obstacleGrid) {
     }
     return dp[m - 1][n - 1];
 };
+
+// 一维空间优化
+var uniquePathsWithObstacles = function (obstacleGrid) {
+    let m = obstacleGrid.length,
+        n = obstacleGrid[0].length;
+    let dp = new Array(n).fill(0);
+    dp[0] = (obstacleGrid[0][0] === 0);
+    for (let i = 0; i < m; ++i) {
+        for (let j = 0; j < n; ++j) {
+            if (obstacleGrid[i][j] === 1) {
+                dp[j] = 0;
+                continue;
+            }
+            if (j - 1 >= 0 && obstacleGrid[i][j - 1] === 0)
+                dp[j] += dp[j - 1];
+        }
+    }
+    return dp[n - 1];
+}
