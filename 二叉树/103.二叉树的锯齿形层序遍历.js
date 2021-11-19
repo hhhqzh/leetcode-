11d1700b8bd74956969f1200022484761796f6cd
hhhqzh <985388100@qq.com>
2021-11-19 21:43:49 +0800

    完成第103題

--- 二叉树/103.二叉树的锯齿形层序遍历.js
+++ 二叉树/103.二叉树的锯齿形层序遍历.js
@@ -10,35 +10,33 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
- var zigzagLevelOrder = function (root) {
+var zigzagLevelOrder = function (root) {
     if (root === null)
         return [];
     let queue = [root];
     let res = [];
     let arr = [];
-    let lastNode = root;
-    let newNode = null;
     let flag = true;
     while (queue.length) {
-        let temp = queue.shift();
-        if (flag)
-            arr.push(temp.val);
-        else
-            arr.unshift(temp.val);
-        if (temp.left !== null) {
-            queue.push(temp.left);
-            newNode = temp.left;
+        let size = queue.length;
+        while (size--) {
+            let temp = queue.shift();
+            if (flag)
+                arr.push(temp.val);
+            else
+                arr.unshift(temp.val);
+            if (temp.left !== null) {
+                queue.push(temp.left);
+                newNode = temp.left;
+            }
+            if (temp.right !== null) {
+                queue.push(temp.right);
+                newNode = temp.right;
+            }
         }
-        if (temp.right !== null) {
-            queue.push(temp.right);
-            newNode = temp.right;
-        }
-        if (lastNode === temp) {
-            lastNode = newNode;
-            res.push([].concat(arr));
-            arr = [];
-            flag = !flag;
-        }
+        res.push([].concat(arr));
+        arr = [];
+        flag = !flag;
     }
     return res;
 };
