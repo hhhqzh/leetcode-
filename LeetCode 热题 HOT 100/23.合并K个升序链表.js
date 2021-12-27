410cda9ce6a1b59b141fa44936f62f137fb81790
hhhqzh <985388100@qq.com>
2021-12-27 11:21:48 +0800

    完成第23题

--- LeetCode 热题 HOT 100/23.合并K个升序链表.js
+++ LeetCode 热题 HOT 100/23.合并K个升序链表.js
@@ -1,26 +1,4 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode[]} lists
- * @return {ListNode}
- */
-
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode[]} lists
- * @return {ListNode}
- */
+// 现在超时了。。。
 var mergeKLists = function (lists) {
     if (lists.length === 0)
         return null;
@@ -48,4 +26,43 @@ var mergeKLists = function (lists) {
         list1 = head.next;
     }
     return head.next;
+};
+
+// 分治思想
+var mergeKLists = function (lists) {
+    if (lists.length === 0)
+        return null;
+    while (lists.length > 1) {
+        let tempLists = [];
+        let i = 0;
+        while (i + 1 < lists.length) {
+            let list1 = lists[i];
+            let list2 = lists[i + 1];
+            i += 2;
+            let head = new ListNode(-1);
+            let pre = head;
+            head.next = list1;
+            while (list1 !== null && list2 !== null) {
+                if (list1.val > list2.val) {
+                    let s = list2.next;
+                    pre.next = list2;
+                    list2.next = list1;
+                    list2 = s;
+                    pre = pre.next;
+                } else {
+                    pre = list1;
+                    list1 = list1.next;
+                }
+            }
+            if (list2 !== null) {
+                pre.next = list2;
+            }
+            tempLists.push(head.next);
+        }
+        if (i < lists.length) {
+            tempLists.push(lists[i]);
+        }
+        lists = [...tempLists];
+    }
+    return lists[0];
 };
