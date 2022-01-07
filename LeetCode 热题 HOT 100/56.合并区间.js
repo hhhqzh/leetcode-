8537601b31721e0621aedf931306a3a0670c240c
hhhqzh <985388100@qq.com>
2022-01-07 10:12:09 +0800

    完成第56题

--- LeetCode 热题 HOT 100/56.合并区间.js
+++ LeetCode 热题 HOT 100/56.合并区间.js
@@ -3,6 +3,22 @@
  * @return {number[][]}
  */
 var merge = function (intervals) {
+    intervals.sort((a, b) => {
+        return a[0] - b[0];
+    });
+    let i = 1;
+    while (i < intervals.length) {
+        if (intervals[i - 1][1] >= intervals[i][0]) {
+            intervals[i - 1][1] = intervals[i - 1][1] > intervals[i][1] ? intervals[i - 1][1] : intervals[i][1];
+            intervals.splice(i, 1);
+        } else {
+            ++i;
+        }
+    }
+    return intervals;
+};
+
+var merge = function (intervals) {
     let tempArr = [].concat(intervals);
     tempArr.sort((a, b) => {
         return a[0] - b[0];
