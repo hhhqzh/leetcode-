f6d57f13d6060c39f9a1a0c8f80f0a52489630e0
hhhqzh <985388100@qq.com>
2022-01-07 09:23:52 +0800

    完成第1614题

---
+++ 1614.括号的最大嵌套深度.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxDepth = function (s) {
+    let max = 0;
+    let count = 0;
+    for (let i = s.length - 1; i >= 0; --i) {
+        if (s.charAt(i) === ')') {
+            ++count;
+            max = Math.max(max, count);
+        } else if (s.charAt(i) === '(') {
+            --count;
+        }
+    }
+    return max;
+};
