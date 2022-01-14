3e07e618080936b80cb03fa52c3bfe59791f5f17
hhhqzh <985388100@qq.com>
2022-01-14 16:07:01 +0800

    完成第373题

---
+++ 373.查找和最小的K对数字.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number} k
+ * @return {number[][]}
+ */
+ var kSmallestPairs = function (nums1, nums2, k) {
+    if (k > nums1.length * nums2.length) {
+        k = nums1.length * nums2.length
+    }
+    if (nums1.length == 0 || nums2.length == 0) {
+        return [];
+    }
+    let steps = new Array(nums1.length);
+    for (let i = 0; i < steps.length; ++i) {
+        steps[i] = 0;
+    }
+    let res = [];
+    for (let i = 0; i < k; ++i) {
+        let min = Number.MAX_SAFE_INTEGER;
+        let minStepIndex = 0;
+        for (let j = 0; j < nums1.length; ++j) {
+            if (steps[j] < nums2.length && nums2[steps[j]] + nums1[j] < min) {
+                min = nums2[steps[j]] + nums1[j];
+                minStepIndex = j;
+            }
+        }
+        res.push([nums1[minStepIndex], nums2[steps[minStepIndex]]]);
+        ++steps[minStepIndex];
+    }
+    return res;
+};
