b71bb2e9c5ca46d699a674195ed753fa6d72ab7c
hhhqzh <985388100@qq.com>
2022-02-03 13:20:23 +0800

    完成102题

---
+++ LeetCode 热题 HOT 100/102.二叉树的层序遍历.js
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+ var levelOrder = function(root) {
+    if (root == null)
+        return [];
+    let queue = [root];
+    let res = [];
+    while (queue.length) {
+        let size = queue.length;
+        let arr = [];
+        while(size--) {
+            let temp = queue.shift();
+            arr.push(temp.val);
+            if (temp.left !== null)
+                queue.push(temp.left);
+            if (temp.right !== null)
+                queue.push(temp.right);
+        }
+        res.push(arr);
+    }
+    return res;
+};
+
+// 使用lastNode来判断当前层是否遍历结束
+var levelOrder = function (root) {
+    if (root === null)
+        return [];
+    let queue = [root];
+    let res = [];
+    let arr = [];
+    let lastNode = root;
+    let newNode = null;
+    while (queue.length) {
+        let temp = queue.shift();
+        arr.push(temp.val);
+        if (temp.left !== null) {
+            queue.push(temp.left);
+            newNode = temp.left;
+        }
+        if (temp.right !== null) {
+            queue.push(temp.right);
+            newNode = temp.right;
+        }
+        if (lastNode === temp) {
+            lastNode = newNode;
+            res.push([].concat(arr));
+            arr = [];
+        }
+    }
+    return res;
+};
