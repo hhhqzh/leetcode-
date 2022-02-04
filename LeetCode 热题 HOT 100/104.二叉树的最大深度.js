9bf476cf11fdee554a641cc7f81a60686e8089fe
hhhqzh <985388100@qq.com>
2022-02-04 21:49:43 +0800

    完成104题

---
+++ LeetCode 热题 HOT 100/104.二叉树的最大深度.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var maxDepth = function(root) {
+    
+    const dfs = (root) => {
+        if (root == null)
+            return 0;
+        if (root.left == null && root.right == null)
+            return 1;
+        return 1 + Math.max(dfs(root.left), dfs(root.right));
+    }
+ 
+    return dfs(root);
+};
