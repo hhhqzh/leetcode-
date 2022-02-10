db5898c81ee0c2c474bade7f94fecad5f62d01a0
hhhqzh <985388100@qq.com>
2022-02-10 11:20:07 +0800

    完成124题

---
+++ LeetCode 热题 HOT 100/124.二叉树中的最大路径和.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var maxPathSum = function(root) {
+    let res = Number.MIN_SAFE_INTEGER;
+
+    const dfs = (root) => {
+        if (root == null)
+            return 0;
+        let left = Math.max(0, dfs(root.left));
+        let right = Math.max(0, dfs(root.right));
+        res = Math.max(res, left + right + root.val);
+        return root.val + Math.max(left, right);
+    }
+
+    dfs(root);
+
+    return res;
+};
