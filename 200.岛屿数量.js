64cf5aa1c6f1fb38c638299ffdc448d4bf390117
hhhqzh <985388100@qq.com>
2022-03-01 15:17:31 +0800

    完成第200题

--- 200.岛屿数量.js
+++ 200.岛屿数量.js
@@ -34,3 +34,76 @@ var numIslands = function (grid) {
 
     return count;
 };
+
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+// 并查集
+class UnionFind {
+    /**
+     * 连通分量的个数
+     */
+    count;
+    parent;
+
+    constructor(grid) {
+        this.count = 0;
+        let row = grid.length,
+            col = grid[0].length;
+        this.parent = new Array(row * col).fill(-1);
+        for (let i = 0; i < row; ++i) {
+            for (let j = 0; j < col; ++j) {
+                if (grid[i][j] === '1') {
+                    this.parent[i * col + j] = i * col + j;
+                    ++this.count;
+                }
+            }
+        }
+    }
+
+    getCount() {
+        return this.count;
+    }
+
+    findParent(x) {
+        while (x != this.parent[x]) {
+            x = this.parent[x];
+        }
+        return x;
+    }
+
+    union(x, y) {
+        let xParent = this.findParent(x);
+        let yParent = this.findParent(y);
+        if (xParent == yParent) {
+            return;
+        }
+
+        this.parent[yParent] = xParent;
+        --this.count;
+    }
+}
+var numIslands = function (grid) {
+    let uni = new UnionFind(grid);
+    let row = grid.length,
+        col = grid[0].length,
+        dires = [
+            [0, 1],
+            [1, 0]
+        ];
+    for (let i = 0; i < row; ++i) {
+        for (let j = 0; j < col; ++j) {
+            if (grid[i][j] === '1') {
+                for (let d of dires) {
+                    let r = i + d[0],
+                        c = j + d[1];
+                    if (r >= 0 && r < row && c >= 0 && c < col && grid[r][c] === '1') {
+                        uni.union(i * col + j, r * col + c);
+                    }
+                }
+            }
+        }
+    }
+    return uni.count;
+};
