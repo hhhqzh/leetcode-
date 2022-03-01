53d8ba7a95f4e2c8176f0d04ca7f271c19ed0a2e
hhhqzh <985388100@qq.com>
2022-03-01 15:48:58 +0800

    完成第4题

--- 二分查找/4.寻找两个正序数组的中位数.js
+++ 二分查找/4.寻找两个正序数组的中位数.js
@@ -34,35 +34,33 @@ var findMedianSortedArrays = function (nums1, nums2) {
     换成查找第k小元素
     取两个数组的第 k/2 个元素进行比较，若 arr1 的小于 arr2 的，则 arr1 的前 k/2 个元素不可能成为第 k 个元素的候选
     把 arr1 的前 k/2 个元素去掉成为新的数组，再与 arr2 一起求第 k - k/2 小的元素
- */ 
+ */
 var findMedianSortedArrays = function (nums1, nums2) {
     let m = nums1.length,
         n = nums2.length;
-
-    const findKth = (arr1, arr2, k) => {
-        let len1 = arr1.length,
-            len2 = arr2.length;
-        // 保证 arr1 的长度一定比 arr2 小
-        if (len1 > len2) {
-            return findKth(arr2, arr1, k);
-        }
+    const findK = (nums1, nums2, k) => {
+        let len1 = nums1.length,
+            len2 = nums2.length;
         if (len1 === 0) {
-            return arr2[k - 1];
+            return nums2[k - 1];
+        }
+        if (len2 === 0) {
+            return nums1[k - 1];
         }
         if (k === 1) {
-            return Math.min(arr1[0], arr2[0]);
+            return Math.min(nums1[0], nums2[0]);
         }
         let i = Math.min(len1, Math.floor(k / 2)) - 1,
             j = Math.min(len2, Math.floor(k / 2)) - 1;
-        if (arr1[i] > arr2[j]) {
-            return findKth(arr1, arr2.slice(j + 1), k - j - 1);
+        if (nums1[i] <= nums2[j]) {
+            return findK(nums1.slice(i + 1), nums2, k - i - 1);
         } else {
-            return findKth(arr1.slice(i + 1), arr2, k - i - 1);
+            return findK(nums1, nums2.slice(j + 1), k - j - 1);
         }
     }
-
-    let left = Math.floor((m + n + 1) / 2),
-        right = Math.floor((m + n + 2) / 2);
-    // 包括奇数偶数的情况
-    return (findKth(nums1, nums2, left) + findKth(nums1, nums2, right)) * 0.5;
+    let l = Math.floor((m + n + 1) / 2),
+        r = Math.floor((m + n + 2) / 2);
+    let a = findK(nums1, nums2, l),
+        b = findK(nums1, nums2, r);
+    return (a + b) * 0.5
 };
