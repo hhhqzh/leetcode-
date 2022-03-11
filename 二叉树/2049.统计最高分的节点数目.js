6e24a9f332b3762e019e265dc79582a5f37deb79
hhhqzh <985388100@qq.com>
2022-03-11 11:19:05 +0800

    完成第2049题

--- 二叉树/2049.统计最高分的节点数目.js
+++ 二叉树/2049.统计最高分的节点数目.js
@@ -34,3 +34,33 @@ var countHighestScoreNodes = function (parents) {
     dfs(son, 0, n);
     return cnt;
 };
+
+// 超时
+var countHighestScoreNodes = function (parents) {
+    let maxScore = 0,
+        n = parents.length,
+        cnt = 0;
+
+    const dfs = (root) => {
+        let a = parents.indexOf(root),
+            b = parents.lastIndexOf(root),
+            left = 0,
+            right = 0;
+        if (a !== -1) {
+            left = dfs(a);
+        }
+        if (b !== -1 && a !== b) {
+            right = dfs(b);
+        }
+        let sum = (left === 0 ? 1 : left) * (right === 0 ? 1 : right) * (n - 1 - left - right === 0 ? 1 : n - 1 - left - right);
+        if (maxScore < sum) {
+            maxScore = sum;
+            cnt = 1;
+        } else if (maxScore === sum) {
+            ++cnt;
+        }
+        return left + right + 1;
+    }
+    dfs(0);
+    return cnt;
+};
