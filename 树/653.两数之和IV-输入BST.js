ec97a273b884b60d680e991e0a30ae03355815c8
hhhqzh <985388100@qq.com>
2022-03-21 10:04:15 +0800

    完成653题

--- 树/653.两数之和IV-输入BST.js
+++ 树/653.两数之和IV-输入BST.js
@@ -25,3 +25,45 @@ var findTarget = function (root, k) {
     }
     return dfs(root, k);
 };
+
+// 利用BST的特点，与数组两数之和类似，双指针保存最大和最小指针
+var findTarget = function (root, k) {
+    let ld = [], // 栈顶保存最小指针
+        rd = []; // 栈顶保存最大指针
+    let node = root;
+    while (node !== null) {
+        ld.push(node);
+        node = node.left;
+    }
+    node = root;
+    while (node !== null) {
+        rd.push(node);
+        node = node.right;
+    }
+    let l = ld[ld.length - 1],
+        r = rd[rd.length - 1];
+    // 与两数之和双指针思路一样
+    while (l.val < r.val) {
+        let sum = l.val + r.val
+        if (sum === k) {
+            return true;
+        } else if (sum > k) {
+            rd.pop();
+            node = r.left;
+            while (node !== null) {
+                rd.push(node);
+                node = node.right;
+            }
+            r = rd[rd.length - 1];
+        } else {
+            ld.pop();
+            node = l.right;
+            while (node !== null) {
+                ld.push(node);
+                node = node.left;
+            }
+            l = ld[ld.length - 1];
+        }
+    }
+    return false;
+}
