101a33bb23dce088d491234250d52e5809b2036a
hhhqzh <985388100@qq.com>
2022-04-02 11:26:30 +0800

    完成第97题

--- 动态规划dp/97.交错字符串.js
+++ 动态规划dp/97.交错字符串.js
@@ -15,16 +15,38 @@ var isInterleave = function (s1, s2, s3) {
         return new Array(n + 1).fill(false)
     });
     dp[0][0] = true;
-    for (let i = 1; i <= m; ++i) {
-        dp[i][0] = dp[i - 1][0] && (s3[i - 1] === s1[i - 1])
-    }
-    for (let i = 1; i <= n; ++i) {
-        dp[0][i] = dp[0][i - 1] && (s3[i - 1] === s2[i - 1]);
-    }
-    for (let i = 1; i <= m; ++i) {
-        for (let j = 1; j <= n; ++j) {
-            dp[i][j] = dp[i - 1][j] && (s3[i + j - 1] === s1[i - 1]) || dp[i][j - 1] && (s3[i + j - 1] === s2[j - 1]);;;
+    for (let i = 0; i <= m; ++i) {
+        for (let j = 0; j <= n; ++j) {
+            if (i > 0) {
+                dp[i][j] |= (dp[i - 1][j] && s3[i + j - 1] === s1[i - 1]);
+            }
+            if (j > 0) {
+                dp[i][j] |= (dp[i][j - 1] && s3[i + j - 1] === s2[j - 1]);
+            }
         }
     }
     return dp[m][n];
+};
+
+
+// 空间优化
+var isInterleave = function (s1, s2, s3) {
+    if (s1.length + s2.length !== s3.length) {
+        return false;
+    }
+    let m = s1.length,
+        n = s2.length;
+    let dp = new Array(n + 1).fill(false);
+    dp[0] = true;
+    for (let i = 0; i <= m; ++i) {
+        for (let j = 0; j <= n; ++j) {
+            if (i > 0) {
+                dp[j] &= s3[i + j - 1] === s1[i - 1];
+            }
+            if (j > 0) {
+                dp[j] |= (dp[j - 1] && s3[i + j - 1] === s2[j - 1]);
+            }
+        }
+    }
+    return dp[n];
 };
